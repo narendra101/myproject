9fba95f9239d0d1c8f2ff30f64153fc76fa75a84
narendra101 <narendra11470@gmail.com>
2022-06-08 23:14:44 +0530

    changed urls

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
 
 def home(request):
     return HttpResponse('welcome')
@@ -9,4 +11,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('todo/', include('todo.urls', namespace="todo"))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
